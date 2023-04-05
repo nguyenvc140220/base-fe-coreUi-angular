4af1068561c84c73938fc93d77571df2c3f04fab
Vũ Cao Nguyên <nguyenvc@metechvn.com>
2023-04-05 09:51:11 +0700

    feature/MKT-33-add-campaign-component

---
+++ src/app/views/campaigns/campaigns-table/campaigns-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaigns-table',
+  templateUrl: './campaigns-table.component.html',
+  styleUrls: ['./campaigns-table.component.scss']
+})
+export class CampaignsTableComponent {
+
+}
