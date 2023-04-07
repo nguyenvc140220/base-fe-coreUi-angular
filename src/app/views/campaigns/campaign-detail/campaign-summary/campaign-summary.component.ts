dcdae220f94be261c468e01068d925fb94fafc6c
__Luong <dot@luongmb.local>
2023-04-07 10:03:09 +0700

    report charts

--- src/app/views/campaigns/campaign-detail/campaign-summary/campaign-summary.component.ts
+++ src/app/views/campaigns/campaign-detail/campaign-summary/campaign-summary.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { BreadcrumbStore } from "@shared/services/breadcrumb.store";
+import { EChartsOption } from "echarts/types/dist/echarts";
+
 
 @Component({
   selector: 'app-campaign-summary',
@@ -10,7 +12,8 @@ import { BreadcrumbStore } from "@shared/services/breadcrumb.store";
 export class CampaignSummaryComponent implements OnInit {
 
   data: any;
-  options: any;
+  options_1: EChartsOption;
+  options_2: EChartsOption;
 
   constructor(route: ActivatedRoute, breadcrumbStore: BreadcrumbStore) {
     const {queryParams} = route.snapshot;
@@ -26,63 +29,137 @@ export class CampaignSummaryComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const documentStyle = getComputedStyle(document.documentElement);
-    const textColor = documentStyle.getPropertyValue('--text-color');
-    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+    const data_1 = [
+      { name: '10/03', notHandled: 200, handled: 2 },
+      { name: '11/03', notHandled: 70, handled: 1 },
+      { name: '12/03', notHandled: 1000, handled: 16 },
+      { name: '13/03', notHandled: 200, handled: 2 },
+      { name: '14/03', notHandled: 500, handled: 11 },
+      { name: '15/03', notHandled: 1450, handled: 19 },
+      { name: '16/03', notHandled: 2690, handled: 69 },
+      { name: '17/03', notHandled: 100, handled: 0 },
+      { name: '18/03', notHandled: 311, handled: 2 },
+      { name: '19/03', notHandled: 200, handled: 2 },
+      { name: '20/03', notHandled: 1245, handled: 41 },
+      { name: '21/03', notHandled: 342, handled: 5 },
+      { name: '22/03', notHandled: 786, handled: 4 },
+      { name: '23/03', notHandled: 875, handled: 10 },
+      { name: '24/03', notHandled: 887, handled: 22 },
+      { name: '25/03', notHandled: 2145, handled: 72 },
+      { name: '26/03', notHandled: 574, handled: 8 },
+      { name: '27/03', notHandled: 357, handled: 11 },
+      { name: '28/03', notHandled: 1355, handled: 69 },
+    ];
 
-    this.data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'My First dataset',
-          backgroundColor: documentStyle.getPropertyValue('--blue-500'),
-          borderColor: documentStyle.getPropertyValue('--blue-500'),
-          data: [65, 59, 80, 81, 56, 55, 40]
+
+
+    const dialed = `Đã gọi`;
+    const interact = `Hứng thú`;
+
+    this.options_1 = {
+      tooltip: {},
+      legend: {
+        data: [dialed, interact]
+      },
+      xAxis: {
+        data: data_1.map(item => item.name),
+      },
+      yAxis: {
+        type: 'value',
+        data: [0, 100000, 200000, 300000, 400000, 500000],
+        axisLabel: {
+          // interval: 0,
+          // rotate: -45,
         },
-        {
-          label: 'My Second dataset',
-          backgroundColor: documentStyle.getPropertyValue('--pink-500'),
-          borderColor: documentStyle.getPropertyValue('--pink-500'),
-          data: [28, 48, 40, 19, 86, 27, 90]
-        }
-      ]
-    };
-
-    this.options = {
-      indexAxis: 'y',
-      maintainAspectRatio: false,
-      aspectRatio: 0.8,
-      plugins: {
-        legend: {
-          labels: {
-            color: textColor
-          }
+        axisTick: {
+          // alignWithLabel: true
         }
       },
-      scales: {
-        x: {
-          ticks: {
-            color: textColorSecondary,
-            font: {
-              weight: 500
-            }
+      series: [
+        {
+          name: dialed,
+          type: 'bar',
+          data: data_1.map(item => item.notHandled),
+          itemStyle: {
+            color: '#3557F6'
           },
-          grid: {
-            color: surfaceBorder,
-            drawBorder: false
-          }
+          barMaxWidth: 36
         },
-        y: {
-          ticks: {
-            color: textColorSecondary
+        {
+          name: interact,
+          type: 'bar',
+          data: data_1.map(item => item.handled),
+          itemStyle: {
+            color: '#D1DFF4'
           },
-          grid: {
-            color: surfaceBorder,
-            drawBorder: false
-          }
+          barMaxWidth: 36
         }
-      }
-    };
+      ]
+    }
+
+    const data_2 = [
+      { name: '10/03', notHandled: 200, handled: 170 },
+      { name: '11/03', notHandled: 70, handled: 36 },
+      { name: '12/03', notHandled: 1000, handled: 544 },
+      { name: '13/03', notHandled: 200, handled: 122 },
+      { name: '14/03', notHandled: 500, handled: 234 },
+      { name: '15/03', notHandled: 1450, handled: 776 },
+      { name: '16/03', notHandled: 2690, handled: 1126 },
+      { name: '17/03', notHandled: 100, handled: 4 },
+      { name: '18/03', notHandled: 311, handled: 234 },
+      { name: '19/03', notHandled: 200, handled: 167 },
+      { name: '20/03', notHandled: 1245, handled: 764 },
+      { name: '21/03', notHandled: 342, handled: 259 },
+      { name: '22/03', notHandled: 786, handled: 475 },
+      { name: '23/03', notHandled: 875, handled: 697 },
+      { name: '24/03', notHandled: 887, handled: 770 },
+      { name: '25/03', notHandled: 2145, handled: 1798 },
+      { name: '26/03', notHandled: 574, handled: 355 },
+      { name: '27/03', notHandled: 357, handled: 314 },
+      { name: '28/03', notHandled: 1355, handled: 766 },
+    ];
+
+    this.options_2 = {
+      tooltip: {},
+      legend: {
+        data: ['Thất bại', 'Thành công']
+      },
+      xAxis: {
+        data: data_2.map(item => item.name),
+      },
+      yAxis: {
+        type: 'value',
+        data: [0, 100000, 200000, 300000, 400000, 500000],
+        axisLabel: {
+          // interval: 0,
+          // rotate: -45,
+        },
+        axisTick: {
+          // alignWithLabel: true
+        }
+      },
+      series: [
+        {
+          name: 'Thất bại',
+          type: 'bar',
+          stack: 'one',
+          data: data_2.map(item => item.notHandled),
+          itemStyle: {
+            color: '#BF73FB'
+          },
+          barMaxWidth: 24
+        },
+        {
+          name: 'Thành công',
+          type: 'bar',
+          stack: 'one',
+          data: data_2.map(item => item.handled),
+          itemStyle: {
+            color: '#73CCF5'
+          },
+          barMaxWidth: 24
+        }
+      ]
+    }
   }
 }
