5b685936576a1dcd76aa4827ca1826fc6d57796c
Vũ Cao Nguyên <nguyenvc@metechvn.com>
2023-04-12 17:32:23 +0700

    fixbug/MKT-79-bug-ui

--- src/app/views/admin/users/user-list/users-list.component.ts
+++ src/app/views/admin/users/user-list/users-list.component.ts
@@ -14,6 +14,10 @@ import { UserModel } from '@shared/models/users/user.model';
 import { UsersService } from '@shared/services/users/users.service';
 import { MenuItem } from 'primeng/api';
 import { UserEditModalComponent } from '../user-edit-modal/user-edit-modal.component';
+import { DynamicQueryModel } from "@shared/models/dynamic-field/dynamic-query.model";
+import { DynamicDataTypeEnum } from "@shared/enums/dynamic-data-type.enum";
+import { DynamicFilterOperatorEnum } from "@shared/enums/dynamic-filter-operator.enum";
+import { DynamicFilterTypeEnum } from "@shared/enums/dynamic-filter-type.enum";
 
 @Component({
   selector: 'app-user-list',
@@ -21,13 +25,28 @@ import { UserEditModalComponent } from '../user-edit-modal/user-edit-modal.compo
   styleUrls: ['./users-list.component.scss'],
 })
 export class UserListComponent
-  extends ComponentBase<UserModel>
-  implements OnInit, OnDestroy
-{
-  cols: any[];
+  extends ComponentBase<any>
+  implements OnInit, OnDestroy {
   searchKey: string = '';
   @ViewChild('paginator') paginator: Paginator;
   items: MenuItem[];
+  query: DynamicQueryModel = {
+    payload: {},
+  };
+  colSearch = [
+    {field: 'username', header: 'Tên đăng nhập'},
+    {field: 'email', header: 'Email'},
+    {field: 'userFullName', header: 'Họ và tên'}
+  ]
+  cols = [
+    {field: 'username', header: 'Tên đăng nhập'},
+    {field: 'email', header: 'Email'},
+    {field: 'userFullName', header: 'Họ và tên'},
+    {field: 'status', header: 'Trạng thái'},
+    {field: 'roles', header: 'Quyền'},
+    {field: 'lastActive', header: 'Ngày hoạt động cuôi'},
+    {field: 'action', header: 'Thao tác'}
+  ]
 
   constructor(
     injector: Injector,
@@ -36,7 +55,7 @@ export class UserListComponent
     private usersService: UsersService
   ) {
     super(injector);
-    breadcrumbStore.items = [{ label: 'Danh sách người dùng' }];
+    breadcrumbStore.items = [{label: 'Danh sách người dùng'}];
   }
 
   ngOnInit(): void {
@@ -46,33 +65,26 @@ export class UserListComponent
   ngOnDestroy(): void {}
 
   private initDataTable() {
-    {
-      this.cols = [
-        { field: 'username', header: 'Tên đăng nhập' },
-        { field: 'fullName', header: 'Họ và tên' },
-        { field: 'email', header: 'email' },
-        { field: 'enable', header: 'Trạng thái' },
-        { field: 'roles', header: 'Quyền' },
-        { field: 'groups', header: 'Nhóm' },
-        { field: 'createdAt', header: 'Ngày tạo' },
-        { field: 'action', header: 'Thao tác' },
-      ];
-    }
     this.loadData(null);
   }
 
   loadData(event) {
+    this.query.currentPage = this.primengTableHelper.getCurrentPage(
+      this.paginator
+    );
+    this.query.pageSize = this.primengTableHelper.getMaxResultCount(
+      this.paginator,
+      event
+    );
     this.primengTableHelper.isLoading = true;
     this.usersService
       .getUsers(
-        this.searchKey ?? '',
-        this.paginator?.currentPage() ?? 1,
-        this.primengTableHelper.defaultRecordsCountPerPage
+        this.query
       )
       .subscribe((res) => {
         this.primengTableHelper.isLoading = false;
-        this.primengTableHelper.records = res.data;
-        this.primengTableHelper.totalRecordsCount = res.total ?? 0;
+        this.primengTableHelper.records = res.data.content;
+        this.primengTableHelper.totalRecordsCount = res.data.totalElements;
       });
   }
 
@@ -84,7 +96,7 @@ export class UserListComponent
     const dialog = this.dialogService.open(UserCreateModalComponent, {
       header: 'Thêm mới người dùng',
       width: '60%',
-      contentStyle: { 'max-height': '80vh', overflow: 'auto' },
+      contentStyle: {'max-height': '80vh', overflow: 'auto'},
     });
     dialog.onClose.subscribe((res) => {
       if (res) {
@@ -97,7 +109,7 @@ export class UserListComponent
     const dialog = this.dialogService.open(UserEditModalComponent, {
       header: 'Chỉnh sửa người dùng',
       width: '60%',
-      contentStyle: { 'max-height': '80vh', overflow: 'auto' },
+      contentStyle: {'max-height': '80vh', overflow: 'auto'},
       data: {
         userId: userId,
       },
@@ -107,5 +119,23 @@ export class UserListComponent
         this.loadData(null);
       }
     });
+  }
+
+  searchData() {
+    if (this.searchKey && this.searchKey.trim() != '') {
+      var payload = this.colSearch.map((c) => {
+        return {
+          field: c.field,
+          operator: DynamicFilterOperatorEnum.CONTAIN,
+          value: this.searchKey.trim(),
+        };
+      });
+      this.query.payload = {
+        type: DynamicFilterTypeEnum.OR,
+        payload: payload,
+      };
+      this.paginator.changePage(1);
+    } else this.query.payload = {};
+    this.loadData(null);
   }
 }
